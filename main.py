50692ac1084111a9ad2fe18ad30fdea36c4e8a90
CodingWithRoyal <asktojatt@gmail.com>
2022-04-12 11:18:15 +0530

    done

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 # This entrypoint file to be used in development. Start by reading README.md
-from time_calculator import add_time, add_hrs
+from time_calculator import add_time, magic_calc
 from unittest import main
 
 
 print(add_time("11:59 PM", "24:05", "Wednesday"))
-
-# print(add_hrs(2, 13, "PM"))
 
 
 # Run unit tests automatically
